05128570c54b7aa914312708958781780f667689
Rupak Yadav <rupakkumar568@gmail.com>
2026-09-13 17:47:47 +0530

    Time: 7 ms (83.09%) | Memory: 47.6 MB (61.96%) - LeetSync

--- 1469-minimum-number-of-steps-to-make-two-strings-anagram/minimum-number-of-steps-to-make-two-strings-anagram.java
+++ 1469-minimum-number-of-steps-to-make-two-strings-anagram/minimum-number-of-steps-to-make-two-strings-anagram.java
@@ -1,16 +1,16 @@
 class Solution {
     public int minSteps(String s, String t) {
-        HashMap <Character , Integer > map = new HashMap<>();
-        int count=0;
-      for (char ch: t.toCharArray()){
-         map.put(ch , map.getOrDefault(ch , 0)+1);
-      }
-      for (char ch:s.toCharArray()){
-       if (map.getOrDefault(ch,0)>0){
-           map.put(ch , map.get(ch)-1);
-       }else
-       count++;
-      }
-     return count;
+        int [] fre = new int [26];
+        for (char ch : s.toCharArray()){
+            fre[ch-'a']++;
+        }
+        int count =0;
+        for (char ch :t.toCharArray()){
+            if (fre[ch-'a']>0){
+                fre[ch-'a']--;
+            }else 
+            count++;
+        }
+        return count ;
     }
 }
